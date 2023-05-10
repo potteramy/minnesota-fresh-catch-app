0c6276dcb75c5514b8dc503558662ca8749b21bb
Sleepy Cat <gilb2966@gmail.com>
2023-05-10 13:51:20 -0500

    added index routes in controllers dir

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const fishRoutes = require('./fishRoutes');
+const lakeRoutes = require('./lakeRoutes');
+
+router.use('/user', userRoutes);
+router.use('/fish', fishRoutes);
+router.use('/lake', lakeRoutes);
+
+module.exports = router;
