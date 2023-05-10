b32161031086fe92c2861211e9b87ebe0ccbdd27
DockDock <ndockman@comcast.net>
2023-05-10 13:38:37 -0500

    created initial fishRoutes.js and userRoutes.js files.

---
+++ controllers/api/fishRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Fish } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const newFish = await Fish.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newFish);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const fishData = await Fish.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!fishData) {
+      res.status(404).json({ message: 'No fish found with this id!' });
+      return;
+    }
+
+    res.status(200).json(fishData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
