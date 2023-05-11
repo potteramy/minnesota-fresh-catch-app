5a19adb7f0273a1febe2d59d86d7ef0c68aa5fc8
Sleepy Cat <gilb2966@gmail.com>
2023-05-11 11:19:16 -0500

    added authorization and session login to current api routes, modded auth.js

---
+++ notes.js
@@ -0,0 +1,15 @@
+/*
+
+Ideas:
+// dynamic webpage load animations (fish-related)
+// chart package
+// file structure; same across all
+// hashed user password
+// restricted to only login users (mod 14 mini project)
+
+Steps:
+// get initial file structure worked out
+// pull down repo
+// assign roles to start setting up routes and models
+
+*/
