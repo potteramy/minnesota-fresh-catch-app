a0a70e6fe0f5af5c11a0dfde3ce4d8d3bfa22675
Jason Triemert <jaytriemert@gmail.com>
2023-05-11 11:47:53 -0500

    added javascript seed data

---
+++ seeds/userSeedData.js
@@ -0,0 +1,20 @@
+[
+  {
+    first_name: 'Hyman',
+    last_name: 'Dixcey',
+    email: 'hdixcey0@princeton.edu',
+    password: '2e1f0b2b0bc7da6fb92ea1a27a1fa74c8791498e'
+  },
+  {
+    first_name: 'Oneida',
+    last_name: 'Stampfer',
+    email: 'ostampfer1@so-net.ne.jp',
+    password: 'ef9c7b2373fd94cc7fa86b81d0501ba3ae7cd98a'
+  },
+  {
+    first_name: 'Elaine',
+    last_name: 'Jonuzi',
+    email: 'ejonuzi2@msu.edu',
+    password: 'd014326a8d51f55d4a429ee7fa0166cc0a5a915f'
+  }
+]
