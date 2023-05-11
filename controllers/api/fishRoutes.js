988e7fe642b1f1e218096212f5e90e1c9c0a10ea
Sleepy Cat <gilb2966@gmail.com>
2023-05-11 11:41:23 -0500

    updated user and fishroutes with session info and auth middleware

--- controllers/api/fishRoutes.js
+++ controllers/api/fishRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { Fish } = require('../../models');
+const withAuth = require("../../utils/auth")
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     const newFish = await Fish.create({
       ...req.body,
@@ -14,7 +15,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
     const fishData = await Fish.destroy({
       where: {
