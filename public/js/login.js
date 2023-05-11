054b6e82752816e33f1979ad73dc0c8914e5d75c
Sleepy Cat <gilb2966@gmail.com>
2023-05-11 12:44:24 -0500

    built login.js in public dir

---
+++ public/js/login.js
@@ -0,0 +1,54 @@
+//we have one event to catch login form data (loginFormHandler), and another to catch signup form data (signupFormHandler); each check for email and password requirements//
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  //if email and password are submitted, send req.body to server to check for correct login credentials
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    //if the response is true, load homepage
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in.');
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+  //this assumes we have a signup form in login.handlebars
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
