45873f26ea45de80a998da8464013ec42d464e78
Potter <potteramylynn@gmail.com>
2023-05-12 11:27:14 -0500

    updated index api from lake to catchroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const fishRoutes = require('./fishRoutes');
-const lakeRoutes = require('./lakeRoutes');
+const lakeRoutes = require('./catchRoutes');
 
 router.use('/user', userRoutes);
 router.use('/fish', fishRoutes);
-router.use('/lake', lakeRoutes);
+router.use('/catch', catchRoutes);
 
 module.exports = router;
