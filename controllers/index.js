d832f8270cd864d51733a764ca6ad1e02adda435
Sleepy Cat <gilb2966@gmail.com>
2023-05-12 13:49:21 -0500

    added code to provide routing to login page

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require("./homeRoutes");
 
 router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
 
 router.get("/", (req, res) => {
   console.log("Router for home page")
   res.render("homepage")
 })
+
 module.exports = router;
