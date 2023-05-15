703aa1272bd3a646d3079d8f57c21ec70bebaa31
Potter <potteramylynn@gmail.com>
2023-05-15 14:11:01 -0500

    added logo

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use("/public", express.static('public'));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
